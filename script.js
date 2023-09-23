606cbbd5061047f170e21919d9be9ab14f61f28f
Troy Toman <troy@seagrass.localdomain>
2023-09-23 08:56:19 -0500

    improve rsvp for with email

--- script.js
+++ script.js
@@ -17,11 +17,26 @@ setInterval(() => {
 
 // RSVP Form Logic
 const form = document.getElementById("rsvp-form");
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const formData = new FormData(form);
-    const name = formData.get("name");
-    const email = formData.get("email");
-    // Store or send this data
-    console.log(`RSVP received from ${name} with email ${email}`);
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  
+  const formData = new FormData(form);
+  const name = formData.get("name");
+  const email = formData.get("email");
+  
+  // Make a POST request to the Netlify function
+  const response = await fetch("/.netlify/functions/sendEmail", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ name, email })
+  });
+  
+  if (response.ok) {
+    console.log("Email sent successfully");
+  } else {
+    console.log("Failed to send email");
+  }
 });
+
